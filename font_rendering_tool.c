376c6257ccaaf19fb3728577ca3558097336e332
sudip <sudipshil9862@gmail.com>
2022-08-11 17:53:24 +0530

    major changes in font_rendering_tool.c - used box instead of grid

--- font_rendering_tool.c
+++ font_rendering_tool.c
@@ -4,8 +4,8 @@
 #define MAX_TEXT_LEN 5000
 char text_buffer[MAX_TEXT_LEN] = "";
 char text_buffer2[MAX_TEXT_LEN] = "";
-const gchar test_text[] = "\tHello World!";
-const gchar test_text2[] = "\tSudip Shil";
+const gchar test_text[] = "Hello World!\nHere I am";
+const gchar test_text2[] = "Sudip Shil";
 GtkLabel *font_test_label1;
 GtkLabel *font_test_label2;
 
@@ -43,93 +43,78 @@ static void font_set_cb2 (GtkFontButton* button, gpointer user_data)
 
 static void app_activate (GApplication *app, gpointer user_data)
 {
-  GtkWidget *window;
-  GtkWidget *table, *label, *picker;
-    window = gtk_application_window_new(GTK_APPLICATION(app));
-    gtk_window_set_title (GTK_WINDOW (window), "Font Comparison");
-    gtk_window_set_default_size(GTK_WINDOW(window), 500, 500);
+	GtkWidget *window;
+	GtkWidget *table, *label, *picker1, *picker2;
+	GtkWidget *boxb1;
+	GtkWidget *boxb2;
+	GtkWidget *dmyl1;//left
+	GtkWidget *dmyr1;//right
+	GtkWidget *dmyu1;//upper
+	GtkWidget *dmyb1;//bottom
+	GtkWidget *dmyc1;//center
+	window = gtk_application_window_new(GTK_APPLICATION(app));
+	gtk_window_set_title (GTK_WINDOW (window), "Font Comparison");
+	gtk_window_set_default_size(GTK_WINDOW(window), 500, 500);
 
-    table = gtk_grid_new ();
-    gtk_widget_set_margin_start (table, 20);
-    gtk_widget_set_margin_end (table, 20);
-    gtk_widget_set_margin_top (table, 20);
-    gtk_widget_set_margin_bottom (table, 20);
-    gtk_grid_set_row_spacing (GTK_GRID (table), 3);
-    gtk_grid_set_column_spacing (GTK_GRID (table), 10);
-    gtk_window_set_child (GTK_WINDOW (window), table);
+	boxb1 = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 5);
+	gtk_window_set_child (GTK_WINDOW (window), boxb1);
 
-  /*  
-    label = gtk_label_new (NULL);
-    gtk_label_set_markup(GTK_WIDGET(label), "<span font=\"18\" color=\"Blue\"><b>Font 1:</b></span>");
-    gtk_widget_set_halign (label, GTK_ALIGN_START);
-    gtk_widget_set_valign (label, GTK_ALIGN_CENTER);
-    gtk_widget_set_hexpand (label, TRUE);
-    gtk_grid_attach (GTK_GRID (table), label, 0, 1, 1, 1);
-*/
-    picker = gtk_font_button_new ();
-    //gtk_grid_attach (GTK_GRID (table), picker, 1, 1, 1, 1);
-    gtk_grid_attach (GTK_GRID (table), picker, 0, 1, 1, 1);
-    g_signal_connect(picker, "font-set", G_CALLBACK(font_set_cb1), NULL);
-    //gtk_widget_set_hexpand (picker, TRUE);
+	dmyl1 = gtk_label_new(NULL); // dummy label for left space
+				     //gtk_label_set_width_chars (GTK_LABEL (dmyl1), 50);
+	gtk_widget_set_hexpand (dmyl1, TRUE);
+	gtk_box_append (GTK_BOX (boxb1), dmyl1);
 
 
-    font_test_label1 = gtk_label_new (NULL);
-    gtk_widget_set_halign (font_test_label1, GTK_ALIGN_START);
-    gtk_widget_set_valign (font_test_label1, GTK_ALIGN_CENTER);
-    //gtk_widget_set_hexpand (font_test_label1, TRUE);
-    //gtk_widget_set_vexpand (font_test_label1, TRUE);
-    //gtk_grid_attach (GTK_GRID (table), GTK_WIDGET(font_test_label1), 1, -1, 1, 1);
-    gtk_grid_attach (GTK_GRID (table), GTK_WIDGET(font_test_label1), 1, 1, 1, 1);
-    set_test_label_text_and_font1("Sans Regular 25");
+	boxb2 = gtk_box_new (GTK_ORIENTATION_VERTICAL, 10);
+	gtk_box_append (GTK_BOX (boxb1), boxb2);
 
 
-    //------------------------------------------------------------------------------------
-/*
-    label = gtk_label_new (NULL);
-    gtk_label_set_markup(GTK_WIDGET(label), "<span font=\"18\" color=\"Blue\"><b>Font 2:</b></span>");
-    gtk_widget_set_halign (label, GTK_ALIGN_START);
-    gtk_widget_set_valign (label, GTK_ALIGN_CENTER);
-    gtk_widget_set_hexpand (label, TRUE);
-    gtk_grid_attach (GTK_GRID (table), label, 0, 3, 1, 1);
-*/
-    picker = gtk_font_button_new ();
-    //gtk_grid_attach (GTK_GRID (table), picker, 1, 3, 1, 1);
-    gtk_grid_attach (GTK_GRID (table), picker, 0, 3, 1, 1);
-    g_signal_connect(picker, "font-set", G_CALLBACK(font_set_cb2), NULL);
-   // gtk_widget_set_hexpand (picker, TRUE);
-
-   
-    font_test_label2 = gtk_label_new (NULL);
-    gtk_widget_set_halign (font_test_label2, GTK_ALIGN_START);
-    gtk_widget_set_valign (font_test_label2, GTK_ALIGN_CENTER);
-    //gtk_widget_set_hexpand (font_test_label2, TRUE);
-    //gtk_widget_set_vexpand (font_test_label2, TRUE);
-    //gtk_grid_attach (GTK_GRID (table), GTK_WIDGET(font_test_label2), 1, 2, 1, 1);
-    gtk_grid_attach (GTK_GRID (table), GTK_WIDGET(font_test_label2), 1, 3, 1, 1);
-    set_test_label_text_and_font2("Sans Regular 25");
+	dmyr1 = gtk_label_new(NULL); // dummy label for right space
+				     //gtk_label_set_width_chars (GTK_LABEL (dmyr1), 50);
+	gtk_widget_set_hexpand (dmyr1, TRUE);
+	gtk_box_append (GTK_BOX (boxb1), dmyr1);
 
 
+	dmyu1 = gtk_label_new(NULL); // dummy label for upper space 
+	gtk_widget_set_vexpand (dmyu1, TRUE);
+	gtk_box_append (GTK_BOX (boxb2), dmyu1);
 
-   //------------------------------------------------------------------------------------
-/*   
-   label = gtk_label_new (NULL);
-    gtk_label_set_markup(GTK_WIDGET(label), "<span font=\"18\" color=\"Blue\"><b>\n\nPango Two Font Description:</b></span>");
-    gtk_widget_set_halign (label, GTK_ALIGN_START);
-    gtk_widget_set_valign (label, GTK_ALIGN_CENTER);
-    gtk_widget_set_hexpand (label, TRUE);
-    gtk_grid_attach (GTK_GRID (table), label, 0, 4, 1, 1);
+	picker1 = gtk_font_button_new ();
+	gtk_box_append (GTK_BOX (boxb2), picker1);
+	g_signal_connect(picker1, "font-set", G_CALLBACK(font_set_cb1), NULL);
+	//gtk_widget_set_hexpand (picker1, FALSE);
 
-    GtkWidget *font_test_label3;
-    font_test_label3 = gtk_label_new (NULL);
-    gtk_label_set_markup(GTK_WIDGET(font_test_label3), "<span font=\"20\" color=\"Green\">|| Font Difference Described Here ||</span>");
-    gtk_widget_set_halign (font_test_label3, GTK_ALIGN_START);
-    gtk_widget_set_valign (font_test_label3, GTK_ALIGN_CENTER);
-    gtk_widget_set_hexpand (font_test_label3, TRUE);
-    gtk_widget_set_vexpand (font_test_label3, TRUE);
-    gtk_grid_attach (GTK_GRID (table), GTK_WIDGET(font_test_label3), 0, 5, 1, 1);
-*/
+	font_test_label1 = gtk_label_new (NULL);
+	gtk_widget_set_halign (font_test_label1, GTK_ALIGN_START);
+	gtk_widget_set_valign (font_test_label1, GTK_ALIGN_CENTER);
+	//gtk_widget_set_hexpand (font_test_label1, TRUE);
+	//gtk_widget_set_vexpand (font_test_label1, TRUE);
+	gtk_box_append (GTK_BOX (boxb2), font_test_label1);
+	set_test_label_text_and_font1("C059 Bold Italic 40");
 
-    gtk_widget_show (window);
+	dmyc1 = gtk_label_new(NULL); // dummy label for center space
+	gtk_widget_set_vexpand (dmyc1, TRUE);
+	gtk_box_append (GTK_BOX (boxb2), dmyc1);
+
+	picker2 = gtk_font_button_new ();
+	gtk_box_append (GTK_BOX (boxb2), picker2); 
+	g_signal_connect(picker2, "font-set", G_CALLBACK(font_set_cb2), NULL);
+	// gtk_widget_set_hexpand (picker2, TRUE);
+
+	font_test_label2 = gtk_label_new (NULL);
+	gtk_widget_set_halign (font_test_label2, GTK_ALIGN_START);
+	gtk_widget_set_valign (font_test_label2, GTK_ALIGN_CENTER);
+	//gtk_widget_set_hexpand (font_test_label2, TRUE);
+	//gtk_widget_set_vexpand (font_test_label2, TRUE);
+	gtk_box_append (GTK_BOX (boxb2), font_test_label2);
+	set_test_label_text_and_font2("C059 Bold 50");
+
+	dmyb1 = gtk_label_new(NULL); // dummy label for bottom space
+	gtk_widget_set_vexpand (dmyb1, TRUE);
+	gtk_box_append (GTK_BOX (boxb2), dmyb1);
+
+
+	gtk_widget_show (window);
 }
 
 
